1484f9ebb97ed894c30772316881493663f6e3ee
Lyn Nagara <lyn.nagara@gmail.com>
2019-04-02 13:05:18 -0700

    drawing

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{console, window, MouseEvent, CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{window, MouseEvent, CanvasRenderingContext2d, HtmlCanvasElement};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 
 pub struct State {
@@ -29,12 +31,14 @@ pub fn init(w: u32, h: u32) -> Result<(), JsValue> {
     body.append_child(&canvas)?;
 
     context.begin_path();
-    let mut is_drawing = false;
+    let state: Rc<RefCell<bool>> = Rc::new(RefCell::new(false));
 
     {
         let context_copy = context.clone();
+        let state_copy = state.clone();
         let handle_mouse_down = Closure::wrap(Box::new(move |event: MouseEvent| {
-            is_drawing = true;
+            let mut is_drawing = state_copy.borrow_mut();
+            *is_drawing = true;
             let new_x = event.x() as f64;
             let new_y = event.y() as f64;
             context_copy.move_to(new_x, new_y);
@@ -50,8 +54,11 @@ pub fn init(w: u32, h: u32) -> Result<(), JsValue> {
 
     {
         let context_copy = context.clone();
+        let state_copy = state.clone();
 
         let handle_mouse_up = Closure::wrap(Box::new(move |event: MouseEvent| {
+            let mut is_drawing = state_copy.borrow_mut();
+            *is_drawing = false;
             let new_x = event.x() as f64;
             let new_y = event.y() as f64;
             context_copy.fill_rect(new_x, new_y, 1.0, 1.0);
@@ -69,11 +76,16 @@ pub fn init(w: u32, h: u32) -> Result<(), JsValue> {
     }
 
     {
+        let context_copy = context.clone();
+        let state_copy = state.clone();
         let handle_mouse_move = Closure::wrap(Box::new(move |event: MouseEvent| {
-            console::log_1(&is_drawing.into());
+            let is_drawing = *state_copy.borrow();
 
             if is_drawing {
-                console::log_1(&"called move".into());
+                let new_x = event.x() as f64;
+                let new_y = event.y() as f64;
+                context_copy.line_to(new_x, new_y);
+                context_copy.stroke();
             }
         }) as Box<dyn FnMut(_)>);
 
