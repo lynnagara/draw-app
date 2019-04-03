38c3c1a86feede3d1d15c771f3199f6de6760045
Lyn Nagara <lyn.nagara@gmail.com>
2019-04-03 12:22:51 -0700

    add clear button

--- src/toolbar.rs
+++ src/toolbar.rs
@@ -34,12 +34,19 @@ pub fn init(toolbar: Element, state: &Rc<RefCell<State>>) -> Result<(), JsValue>
     for size in PEN_SIZES.iter() {
         let el = document.create_element("div")?;
 
-        el.set_attribute("style", "height: 50px; display: flex; align-items: center; justify-content: center;");
+        el.set_attribute(
+            "style",
+            "height: 50px; display: flex; align-items: center; justify-content: center;",
+        );
         toolbar.append_child(&el)?;
 
         let inner_el = document.create_element("div")?;
 
-        let style = format!("border-radius: 50%; background-color: black; width: {}px; height: {}px;", size + 2.0, size + 2.0);
+        let style = format!(
+            "border-radius: 50%; background-color: black; width: {}px; height: {}px;",
+            size + 2.0,
+            size + 2.0
+        );
         inner_el.set_attribute("style", &style);
         el.append_child(&inner_el);
 
@@ -52,6 +59,13 @@ pub fn init(toolbar: Element, state: &Rc<RefCell<State>>) -> Result<(), JsValue>
         el.add_event_listener_with_callback("click", handle_click.as_ref().unchecked_ref())?;
 
         handle_click.forget();
+    }
+
+    {
+        let el = document.create_element("div")?;
+        el.set_attribute("style", "height: 50px; display: flex; align-items: center; justify-content: center; font-size: 11px;");
+        el.set_inner_html("clear");
+        toolbar.append_child(&el)?;
     }
 
     Ok(())
